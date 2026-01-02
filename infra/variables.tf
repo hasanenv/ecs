2f4f2630d2ba3aa52076a3ead51d300965e19b8f
Hasan Kamran <hasan8812011@hotmail.co.uk>
2026-01-02 00:46:07 +0000

    chore(terraform): declare defaults for variables

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,6 @@
+# -------------------------
+# Network (explicit inputs)
+# -------------------------
 variable "vpc_cidr" {
   type = string
 }
@@ -10,17 +13,27 @@ variable "private_subnet_cidrs" {
   type = list(string)
 }
 
+# -------------------------
+# Environment / policy
+# -------------------------
 variable "aws_region" {
-  type = string
-}
-variable "owner" {
-  type = string
+  type    = string
+  default = "eu-west-2"
 }
 
 variable "availability_zones" {
-  type = list(string)
+  type    = list(string)
+  default = ["eu-west-2a", "eu-west-2b"]
 }
 
+variable "owner" {
+  type    = string
+  default = "hasan"
+}
+
+# -------------------------
+# Deployment
+# -------------------------
 variable "image_tag" {
   type        = string
   description = "Docker image tag for ECS task definition"
