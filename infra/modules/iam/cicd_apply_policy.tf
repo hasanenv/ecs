cac9838dc233485c7728e8b00dcb1ad6e0772cd1
Hasan Kamran <hasan8812011@hotmail.co.uk>
2026-01-03 23:45:01 +0000

    feat(iam): manage CI/CD role policies in Terraform

---
+++ infra/modules/iam/cicd_apply_policy.tf
@@ -0,0 +1,88 @@
+resource "aws_iam_policy" "cicd_apply" {
+  name        = "hasanenv-cicd-apply"
+  description = "Write permissions for ECS deployments and Terraform state (CI/CD)"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "ECSDeploy"
+        Effect = "Allow"
+        Action = [
+          "ecs:RegisterTaskDefinition",
+          "ecs:DeregisterTaskDefinition",
+          "ecs:UpdateService",
+          "ecs:DescribeServices",
+          "ecs:DescribeClusters",
+          "ecs:DescribeTaskDefinition",
+          "ecs:DescribeTasks",
+          "ecs:TagResource",
+          "ecs:UntagResource"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "ECRPushPull"
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchGetImage",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:PutImage",
+          "ecr:DescribeRepositories",
+          "ecr:BatchCheckLayerAvailability"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "TerraformState"
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::hasanenv-tf-state",
+          "arn:aws:s3:::hasanenv-tf-state/*"
+        ]
+      },
+      {
+        Sid    = "StateLocking"
+        Effect = "Allow"
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:PutItem",
+          "dynamodb:DeleteItem"
+        ]
+        Resource = "arn:aws:dynamodb:eu-west-2:727646481331:table/terraform-state-lock"
+      },
+      {
+        Sid    = "Route53Changes"
+        Effect = "Allow"
+        Action = ["route53:ChangeResourceRecordSets"]
+        Resource = "arn:aws:route53:::hostedzone/Z0286219X2C9UMZ2LRUH"
+      },
+      {
+        Sid    = "PassExecutionRole"
+        Effect = "Allow"
+        Action = ["iam:PassRole"]
+        Resource = "arn:aws:iam::727646481331:role/ecsTaskExecutionRole"
+      },
+      {
+        Sid    = "LogsWrite"
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:PutRetentionPolicy",
+          "logs:DescribeLogGroups",
+          "logs:ListTagsForResource"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
--- infra/modules/iam/variables.tf
+++ infra/modules/iam/variables.tf
@@ -1,3 +1,8 @@
+variable "cicd_role_name" {
+  description = "Existing CI/CD IAM role name assumed via OIDC (created outside via console)."
+  type        = string
+}
+
 variable "owner" {
   type = string
 }
